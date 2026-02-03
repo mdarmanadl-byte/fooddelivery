aa2ca699aa0e656df24dbfbd4e53cee179244f73
mdarmanadl-byte <mdarmanappleid@gmail.com>
2026-02-03 09:52:24 +0530

    background color fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en " className="dark">
       <body
-        
+        className="bg-amber-800"
       >
        
         {children}
