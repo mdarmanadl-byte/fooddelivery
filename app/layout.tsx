302d77e82810addd85d232acb22f22b0bc286d25
mdarmanadl-byte <mdarmanappleid@gmail.com>
2026-02-03 10:33:57 +0530

    background color fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en " className="dark">
       <body
-        className="bg-amber-800"
+        className="bg-black"
       >
        
         {children}
